8469d8e2a5828850947c60a13026f201147bbd74
Christoffer Langstrøm Knudsen <christoflk@uia.no>
2023-05-24 00:46:57 +0200

    King of fixed a test for add note. Needs some fixing. Adding date check and move to a file called integration tests.

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -39,9 +39,32 @@ def test_admin_login(client):
     assert b"<b>ADMIN</b>" in response.data, "Test not found in navigation bar" # Not the best way, but the website contains no cookies or such
 
 
+"""
+Test for checking if unauthenticated users
+can access the private page.
+"""
 def test_private(client):
     login(client, "test", "123456")
     response = client.get("/private/")
 
     assert response.status_code == 200, "Response code should be 200. Client is not authorized"
     assert b'<h1>Private Page</h1>' in response.data, "Could not enter private page"
+
+
+# TODO: More of an integrationtest, move when necessary. Add datetime check
+def test_add_note(client):
+    login(client, "test", "123456")
+
+    test_private(client)
+
+    note = b'This is the second test note.'
+
+    client.post("/write_note", data={
+        "text_note_to_take": note,
+    }, follow_redirects=True)
+
+    response = client.get("/private/")
+
+    print(response)
+    assert note in response.data, "Note was not posted/found." # Passes only if a part of the text is found on the screen. Should be fixed.
+
